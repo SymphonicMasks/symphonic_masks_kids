2203e8f9cb0993a1e10c98ea862567cb04e5d202
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2024-01-25 16:20:31 +0300

    Add svg writing

--- kids_app/infrastructure/__init__.py
+++ kids_app/infrastructure/__init__.py
@@ -1,7 +1,21 @@
 from pathlib import Path
+import os
 
+from music21 import environment
+
+from kids_app import settings
 from .audio_process.pitch_detection import BasicPitcher
 from .sheet_music.score_generation import SheetGenerator
+from .sheet_music.music_submission import MusicSubmission
+
+us = environment.UserSettings()
+us_path = us.getSettingsPath()
+if not os.path.exists(us_path):
+    us.create()
+
+us['musescoreDirectPNGPath'] = settings.musescoreDirectPNGPath
+us['musicxmlPath'] = settings.musicxmlPath
+us['lilypondPath'] = settings.lilypondPath
 
 basic_pitcher = BasicPitcher(Path("data/midi/temp.midi"))
-music21_renderer = SheetGenerator([0.25, 0.5, 1, 2, 3, 4], Path("data/xml/temp.xml"))
+music21_renderer = SheetGenerator([0.5, 1, 4], [1], Path("data/xml/temp.xml"))
--- kids_app/infrastructure/sheet_music/music_submission.py
+++ kids_app/infrastructure/sheet_music/music_submission.py
@@ -1,10 +1,10 @@
 import copy
 from pathlib import Path
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from dataclasses import dataclass
 
 import pretty_midi
-from music21 import stream, chord
+from music21 import stream, chord, converter
 from music21.note import Note
 
 
@@ -13,7 +13,7 @@ class MusicSubmission:
     original_stream: stream.Stream
     user_notes: List[pretty_midi.Note]
     tempo: int
-    viz_path: Path
+    viz_path: str
 
     def create_skeleton(self) -> Tuple[List[str], List[float]]:
         stream_notes = self.original_stream.notes
@@ -27,14 +27,19 @@ class MusicSubmission:
 
         return original_notes, fractions
 
-    def make_viz(self):
+    def make_viz(self, make_svg: bool = False):
         original_notes, fractions = self.create_skeleton()
         stream_error = copy.deepcopy(self.original_stream)
 
         notes_in_one_sec = self.tempo / 60
         one_time = round(1 / notes_in_one_sec, 2)
+        name = pretty_midi.note_number_to_name(self.user_notes[0].pitch)
 
         results = []
+
+        while name != original_notes[0]:
+            self.user_notes.pop(0)
+            name = pretty_midi.note_number_to_name(self.user_notes[0].pitch)
 
         for i, _note in enumerate(self.user_notes):
             if i >= len(original_notes):
@@ -45,10 +50,27 @@ class MusicSubmission:
             note_time = _note.end - _note.start
             note_fraction = note_time / one_time
 
+            if note_fraction < 0.1:
+                print("continue if < 0.2", note_fraction)
+                continue
+
             if name != original_notes[i]:
                 if i + 1 < len(self.user_notes):
                     next_name = pretty_midi.note_number_to_name(self.user_notes[i + 1].pitch)
+
                     if next_name == original_notes[i]:
+                        continue
+
+                    elif i + 1 < len(original_notes):
+                        if name == original_notes[i + 1]:
+                            error = f"DUR_{note_fraction != fractions[i]}"
+                            stream_error.notes[i].style.color = "yellow"
+                            results.append({"index": i, "note": name, "duration": note_fraction, "error": error})
+
+                            continue
+                if i > 0:
+                    prev_name = pretty_midi.note_number_to_name(self.user_notes[i - 1].pitch)
+                    if prev_name == name:
                         continue
                 error = "NOTE"
                 wrong_note = Note(name, quarterLength=fractions[i])
@@ -69,4 +91,9 @@ class MusicSubmission:
             results.append({"index": i, "note": name, "duration": note_fraction, "error": error})
 
         stream_error.write('musicxml', fp=self.viz_path)
+        if make_svg:
+            svg = self.viz_path.replace(".xml", ".svg")
+            conv = converter.subConverters.ConverterLilypond()
+            conv.write(stream_error, fmt='lilypond', fp=svg, subformats=['svg'])
+
         return results
