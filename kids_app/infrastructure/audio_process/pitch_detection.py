ee971b7c35f22549fed08c4fd96846c7744dc799
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2024-01-31 15:16:13 +0300

    Add frequency limitation

--- kids_app/infrastructure/audio_process/pitch_detection.py
+++ kids_app/infrastructure/audio_process/pitch_detection.py
@@ -9,7 +9,10 @@ class BasicPitcher:
         self.default_path = default_path
 
     def __call__(self, audio_file_path: Union[Path, str], output_path: Optional[Union[Path, str]]):
-        model_output, midi_data, note_events = predict(audio_file_path, onset_threshold=0.6)
+        model_output, midi_data, note_events = predict(audio_file_path,
+                                                       onset_threshold=0.6,
+                                                       minimum_frequency=130.813,
+                                                       maximum_frequency=1278.75)
 
         if output_path is None:
             output_path = self.default_path
@@ -17,5 +20,3 @@ class BasicPitcher:
         midi_data.write(output_path)
 
         return midi_data
-
-
